6b2bfd54f206a72d1918ce1d7bf0c8a8c0a4f888
Altemilson Lopes <altemilsonlopes@gmail.com>
2023-06-10 23:11:02 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Entrada Válida ---");
+
+int numero = -1;
+
+while (numero < 0 || numero >9)
+{
+    Console.WriteLine("Digite um número entre 1 e 9 (ou 0 para cancelar)...:");
+    numero = Convert.ToInt32(Console.ReadLine()!.Trim());
+}
+
+if (numero == 0)
+{
+    Console.WriteLine("Operação Cancelada.");
+    Console.WriteLine("Volte Sempre!");
+}
+else
+{
+    Console.WriteLine($"Numero Selecionado: {numero}");
+}
